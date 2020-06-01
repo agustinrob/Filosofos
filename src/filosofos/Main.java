39de0a978041a03fccfde1f234421f17d5c69dad
agustinrob <agustinrobayna28@gmail.com>
2020-06-01 11:19:58 -0300

    V final, el ultimo filosofo agarra el cubierto de la derecha primero para evitar deadlock

--- src/filosofos/Main.java
+++ src/filosofos/Main.java
@@ -1,4 +1,5 @@
 package filosofos;
+
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -15,8 +16,7 @@ public class Main {
         void agarrar() {
             try {
                 mutex.acquire(1);
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace(System.out);
             }
         }
@@ -36,46 +36,49 @@ public class Main {
         public int number;
         public Cubierto izq;
         public Cubierto der;
-        public Semaphore comedor;
+        public boolean esUltimo;
 
-        Filosofo(int num, Cubierto izq, Cubierto der, Semaphore comedor) {
+        Filosofo(int num, Cubierto izq, Cubierto der, boolean esUltimo) {
             number = num;
             this.izq = izq;
             this.der = der;
-            this.comedor = comedor;
+            this.esUltimo = esUltimo;
         }
 
         @Override
-        public void run(){
-            
+        public void run() {
+
             while (true) {
-                System.out.println("Comedor tiene: " + comedor.availablePermits() + " permits.");
-                try{
-                    this.comedor.acquire(1);
+                if (this.esUltimo) {
+                    der.agarrar();
+                    System.out.println("Filosofo " + (number + 1) + " toma cubierto der.");
+                    izq.agarrar();
+                    System.out.println("Filosofo " + (number + 1) + " toma cubierto izq.");
+                    comer();
+                    izq.soltar();
+                    System.out.println("Filosofo " + (number + 1) + " suelta cubierto izq.");
+                    der.soltar();
+                    System.out.println("Filosofo " + (number + 1) + " suelta cubierto der.");
+                } else {
+                    izq.agarrar();
+                    System.out.println("Filosofo " + (number + 1) + " toma cubierto izq.");
+                    der.agarrar();
+                    System.out.println("Filosofo " + (number + 1) + " toma cubierto der.");
+                    comer();
+                    izq.soltar();
+                    System.out.println("Filosofo " + (number + 1) + " suelta cubierto izq.");
+                    der.soltar();
+                    System.out.println("Filosofo " + (number + 1) + " suelta cubierto der.");
                 }
-                catch(InterruptedException e){
-                    e.printStackTrace(System.out);
-                }
-                izq.agarrar();
-                System.out.println("Filosofo " + (number+1) + " toma cubierto izq.");
-                der.agarrar();
-                System.out.println("Filosofo " + (number+1) + " toma cubierto der.");
-                comer();
-                izq.soltar();
-                System.out.println("Filosofo " + (number+1) + " suelta cubierto izq.");
-                der.soltar();
-                System.out.println("Filosofo " + (number+1) + " suelta cubierto der.");
-                this.comedor.release(1); //como y me voy del comedor
             }
         }
 
         void comer() {
             try {
                 int sleepTime = ThreadLocalRandom.current().nextInt(0, 1000);
-                System.out.println("Filosofo " + (number+1) + " come por " + sleepTime);
+                System.out.println("Filosofo " + (number + 1) + " come por " + sleepTime);
                 Thread.sleep(sleepTime);
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace(System.out);
             }
         }
@@ -85,44 +88,25 @@ public class Main {
     public static void main(String argv[]) {
 
         Semaphore comedor = new Semaphore(4);
-        
+
         System.out.println("comedor: " + comedor);
-        
+
         for (int i = 0; i < cant; i++) {
             cubiertos[i] = new Cubierto();
         }
 
-        for (int i = 0; (i < cant) && (comedor.availablePermits() > 0); i++) {        
-            filosofos[i] = new Filosofo(i, cubiertos[i], cubiertos[(i + 1) % cant], comedor);
+        for (int i = 0; (i < cant); i++) {
+            filosofos[i] = new Filosofo(i, cubiertos[i], cubiertos[(i + 1) % cant], (i == cant - 1));
             filosofos[i].start();
         }
 
         while (true) {
             try {
-                // sleep 1 sec
                 Thread.sleep(1000);
-
-                // check for deadlock
-                boolean deadlock = true;
-                for (Cubierto f : cubiertos) {
-                    if (f.estaLibre()) {
-                        deadlock = false;
-                        break;
-                    }
-                }
-                if (deadlock) {
-                    Thread.sleep(1000);
-                    System.out.println("Todos comen");
-                    break;
-                }
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace(System.out);
             }
         }
-
-        System.out.println("Salir");
-        System.exit(0);
     }
 
 }
